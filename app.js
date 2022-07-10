a32a2276c652a6e75c488e29142b901f61373984
JokerRoller <BECOOL686868@gmail.com>
2022-07-10 18:48:15 +0400

    some

---
+++ app.js
@@ -0,0 +1,15 @@
+// Leanguage detection
+// const options = {
+// 	method: 'POST',
+// 	headers: {
+// 		'content-type': 'application/json',
+// 		'X-RapidAPI-Key': '54b34002bemsh83765f641ced1d4p10956ejsn932c47e21a92',
+// 		'X-RapidAPI-Host': 'google-translate78.p.rapidapi.com'
+// 	},
+// 	body: '{"text":"hello, how are you?"}'
+// };
+
+// fetch('https://google-translate78.p.rapidapi.com/language_detect', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
